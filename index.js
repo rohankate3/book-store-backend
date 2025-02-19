8ddc77b9fcc714452b0b694b6ddb14e8bac6afea
[Rohan Kate] <[rohankate1313@gmail.com]>
2025-02-19 23:36:25 +0530

    changed url

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://book-app-frontend-tau.vercel.app'],
-    credentials: true
+  origin: ['http://localhost:5173', 'https://book-store-frontend-sigma-silk.vercel.app/'],
+  credentials: true
 }))
 
 // routes
 const bookRoutes = require('./src/books/book.route');
 const orderRoutes = require("./src/orders/order.route")
-const userRoutes =  require("./src/users/user.route")
+const userRoutes = require("./src/users/user.route")
 const adminRoutes = require("./src/stats/admin.stats")
 
 app.use("/api/books", bookRoutes)
