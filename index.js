a1f8d628f8b5cd9b32caf1d1f4dd6e69e5dc7d87
[Rohan Kate] <[rohankate1313@gmail.com]>
2025-02-25 23:59:43 +0530

    redeployed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-  origin: ['https://book-store-frontend-sigma-silk.vercel.app'],
+  origin: ['http://localhost:5173', 'https://book-store-frontend-olive-sigma.vercel.app/'],
   credentials: true
 }))
 
