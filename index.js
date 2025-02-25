a547d20fee6d7d542f523b5767df9aeec1e5416d
[Rohan Kate] <[rohankate1313@gmail.com]>
2025-02-25 23:52:19 +0530

    cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://book-store-frontend-sigma-silk.vercel.app'],
+  origin: ['https://book-store-frontend-sigma-silk.vercel.app'],
   credentials: true
 }))
 
